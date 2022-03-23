0e684e96dd82c6b31d781e654729f8006fddf374
StaticCloud <diegomarrs@gmail.com>
2022-03-22 23:31:14 -0700

    conditional page changing

--- src/App.js
+++ src/App.js
@@ -1,15 +1,32 @@
 import './App.css';
 import Header from './components/Header';
+import About from './components/About'
 import { useState } from 'react';
 
 const App = () => {
 
-  const pages = ['About', 'Portfilio', 'Contact', 'Résumé'];
+  const pages = ['About', 'Portfolio', 'Contact', 'Résumé'];
+
+  const [currentPage, setCurrentPage] = useState(pages[0])
 
   return (
-    <Header
-      pages={pages}
-    />
+    <>
+      <Header
+        pages={pages}
+        currentPage={currentPage}
+        setCurrentPage={setCurrentPage}
+      />
+      <main>
+        {(() => {
+          switch(currentPage) {
+            case pages[0]:
+              return (
+                <About/>
+              )
+          }
+        })()}
+      </main>
+    </>
   );
 }
 
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,9 +1,15 @@
-const Header = ({ pages }) => {
+const Header = ({ pages, currentPage, setCurrentPage }) => {
     return (
         <header>
             <h1><a href="/">Diego Marrs</a></h1>
             <ul>
-                {pages.map(page => <li>{page}</li>)}
+                {pages.map(page => 
+                    <li key={page}
+                        className={`${currentPage === page && 'active'}`}
+                        onClick={() => {setCurrentPage(page)}}>
+                        {page}
+                    </li>
+                )}
             </ul>
         </header>
     );
