3d6630366df1812479f694396365edb5fa717db8
StaticCloud <diegomarrs@gmail.com>
2022-03-23 19:40:07 -0700

    about page created!

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,7 +1,19 @@
 const About = () => {
     return (
-        <h1></h1>
-    )
+        <div className="content">
+            <div className="about">
+                <div>
+                    <div className="portrait"/>
+                </div>
+                <div id="about-content">
+                    <h1>About</h1>
+                    <p>
+                        Hello, I'm Diego! I'm a web developer with a certificate in full-stack web development from University of Oregon and an Associate Degree for computer science from Clark College in Vancouver, Washington. I have experience with HTML, CSS, JavaScript, Node.js, Sequelize, Express.js, React, MongoDB, and React. Former experience with data structures and object-oriented programming. <br/> <br/> A notable project of mine was a Portland itinerary for tourists using jQuery and the Bulma CSS library, as well as utilizing a weather API and an API for local attractions. Also developed an application that gives you recipes based on what's available inside of your pantry. Open to new and exciting challenges that create opportunities for leveraging skills. Driven by feedback from coworkers and customers alike. Specializes in intuitive and visually-engaging user experiences.
+                    </p>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default About;
