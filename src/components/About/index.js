f1c773c0007a91e56b0551b6222501851f88192e
StaticCloud <diegomarrs@gmail.com>
2022-04-28 23:32:52 -0700

    included email in about

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -21,6 +21,11 @@ const About = ({ pageChanged }) => {
                     <h1>About</h1>
                     <p style={{minWidth: "360px", maxWidth: "770px"}}>
                         Hello, I'm Diego! I'm a web developer with a certificate in full-stack web development from University of Oregon and an Associate Degree for computer science from Clark College in Vancouver, Washington. I have experience with HTML, CSS, JavaScript, Node.js, Sequelize, Express.js, React, MongoDB, React, GraphQL, and Redux. Former experience with data structures and object-oriented programming. <br/> <br/> A notable project of mine was a Portland itinerary for tourists using the RapidWeb API, the Travel Advisor API, and the Bulma CSS library. Also developed an application that gives you recipes based on what's available inside of your pantry. My best work to date is Beet Hub, a platform created using the MERN stack for sharing and reviewing music albums! Open to new and exciting challenges that create opportunities for leveraging skills. Driven by feedback from coworkers and customers alike. <br/><br/> As someone who enjoys web development and generally creating things, I'm always open to learning new web development skills. I'm a great team player who loves receiving feedback as well as sharing some feedback of my own. I write Github Gists as an opportunity to learn new technical skills in my field, and to assist others with leveraging their skills.
+                        <br/>
+                        <br/>
+                        Reach out to me at:
+                        <br/>
+                        <h3><i><a type="email" href="mailto: stacloud@protonmail.com">stacloud@protonmail.com</a></i></h3>
                     </p>
                 </div>
             </div>
