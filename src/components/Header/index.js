9156539a2a5c30e1309eda826e4e97155843ea21
StaticCloud <diegomarrs@gmail.com>
2022-05-02 22:17:10 -0700

    scrollbar styling

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -18,7 +18,16 @@ const Header = ({ pages, currentPage, setCurrentPage, hasPageChanged }) => {
     return (
         <header>
             <div id="logo-wrap">
-                <h1><a href="/">Diego Marrs</a></h1>
+                <h1 style={{cursor: 'pointer'}} onClick={() => {
+                    // conditionally change the page to about if the current page is not about
+                    {currentPage !== pages[0] && 
+                        hasPageChanged(true);
+                        setTimeout(() => {
+                            setCurrentPage(pages[0]);
+                            hasPageChanged(false);
+                        }, 400)
+                    }
+                }}>Diego Marrs</h1>
                 <h1 id="menu" onClick={() => setMenuOpen(!menuOpen)}>
                     {!menuOpen ? 
                         <i className="bi bi-list"></i> : <i className="bi bi-x"></i>
